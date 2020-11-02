e64badd5516dc56dbb2fe34ab8d785942d57ea33
Gilad Maoz <gilad.maoz@gmail.com>
2020-11-02 10:16:54 +0100

    Hotfix - missing cors middleware dependency

--- src/server.js
+++ src/server.js
@@ -5,16 +5,14 @@ module.exports = (() => {
      * @type {{handler}|*}
      */
     const {baseDir} = require("./base");
-    const fs = require('fs');
     const express = require('express');
-    const cors = require('cors')
     const bodyParser = require('body-parser');
     const serverName = require(`${baseDir}/package.json`).name
     const logger = require('bunyan').createLogger({name: serverName, level: isDev ? 'debug' : 'error'});
     const server = express({name: serverName, log: logger});
     const router = express.Router();
     const baseHandlersFolder = `${baseDir}/src/handlers`;
-    fs.readdirSync(baseHandlersFolder).forEach((fileName) => {
+    require('fs').readdirSync(baseHandlersFolder).forEach((fileName) => {
         if (fileName.includes('.js')) {
             const HandlerImport = require(`${baseHandlersFolder}/${fileName}`);
             Object.keys(HandlerImport).forEach((handler) => {
@@ -35,6 +33,6 @@ module.exports = (() => {
         .use(bodyParser.json())
         .use(bodyParser.urlencoded({extended: true}))
         .use(require('express-bearer-token')())
-        .use(cors({origin: true}))
+        .use(require('cors')({origin: true}))
         .use(isDev ? '/api': '', router)
 })();
